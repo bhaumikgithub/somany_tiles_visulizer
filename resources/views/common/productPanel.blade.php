8dd5dc51804b7cae52226ea56727317780b9b4b2
Vikesha <Vikesha.gandhi@protovate.com>
2025-01-08 17:28:22 +0530

    task done

--- resources/views/common/productPanel.blade.php
+++ resources/views/common/productPanel.blade.php
@@ -7,9 +7,12 @@
     <div class="col-md-12">
 
     <div class="top-panel-box">
-        <h5 class="text-center panel-title">Wall A </h5>
+    <div  class="top-panel-hide-layer">
+        <span id="topPanelHideIcon" class="glyphicon-menu-right glyphicon" aria-hidden="true"></span>
+    </div>
+        <h5 class="text-center panel-title" id="optionText"> </h5>
         <div class="input-box d-flex flex-wrap">
-            <div class="input-text-box-wrap">
+            <div class="input-text-box-wrap" >
         <input id="inputSearch" type="search" value="" placeholder="@lang('Search Product')" class="input-search product-input-serch"><button id="btnSearchIcon" class="search-icon-button">
             <svg class="svg-inline--fa fa-magnifying-glass form-control-feedback" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="magnifying-glass" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path></svg>
         </button>
