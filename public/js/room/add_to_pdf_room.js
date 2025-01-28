382adcf733a92091a2c9cb730b49a624f11e85a0
Kinjal Upadhyay <kinjal.upadhyay@protovate.com>
2025-01-28 12:18:09 +0530

    merged live changes made by bhaumik sir

--- public/js/room/add_to_pdf_room.js
+++ public/js/room/add_to_pdf_room.js
@@ -81,6 +81,7 @@ function canvasImage() {
 }
 
 // Initialize an empty array
+let ids = [];
 function getTileId(id){
     let surface_title = $('#slected-panel .display_surface_name h5#optionText').text();
     let current_room_type = $('#current_room_type').val();
@@ -190,7 +191,7 @@ function clearAllItems() {
             // Optionally update the UI (e.g., empty the cart display)
             $('.productCount').text('');
             $('#addToCartInfoPanelModal #cartInfoTilesList').html(''); // Assuming cart items are listed in #cart-items
-          ;
+            ;
         },
         error: function (xhr) {
             alert('Something went wrong!');
@@ -417,8 +418,39 @@ $("#calculate_btn").click(function () {
     displayResult("#area_covered_meter","Total Area covered : <b>" + totalAreaSqMeter.toFixed(2)+"</b> Sq. Meter");
     displayResult("#area_covered_feet","Total Area covered : <b>" + totalArea.toFixed(2)+"</b> Sq. Feet");
     displayResult("#required_tiles","Required Tiles : <b>" + tilesNeeded+"</b> Tiles");
-});
 
+
+
+    //calculateBoxCoverageArea();
+});
+//0 = Sr. No
+//1 = Name
+//2 = Size
+//3 = Finish
+
+function calculateBoxCoverageArea(){
+    var rows = $(".summary-page-table").find("tr");
+    for(var i=0;i<rows.length;i++){
+        var tileSizeInString= String(findDataFromTable(i,2)); // Size
+        if(tileSizeInString.length>2){
+            var value = tileSizeInString.split("x");
+            var tilesWidthinSqFt = (Number(value[0])/30)/10;
+            var tilesHeightinSqFt = (Number(value[1])/30)/10;
+            findDataFromTable(i,7,Number(tilesWidthinSqFt*tilesHeightinSqFt));//7 = Box Coverage Area in Sq ft
+        }
+    }
+}
+function findDataFromTable(p_row,p_col,p_value){
+    var rows = $(".summary-page-table").find("tr");
+    var row = $(rows).eq(p_row);
+    var foundedRowCol =  $(row).find("td")[p_col];//Size
+    if(p_value){
+        $(foundedRowCol).html(p_value);
+    }
+    else{
+        return $(foundedRowCol).html();
+    }
+}
 
 $('#closeTileCalcModal').click(function() {
 
@@ -696,3 +728,5 @@ function checkCartHasData(){
         });
     }
 }
+
+
