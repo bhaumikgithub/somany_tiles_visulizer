9cf009e261038b2245db927fe171f51b6842b8f9
Kinjal Upadhyay <kinjal.upadhyay@protovate.com>
2025-02-13 15:01:06 +0530

    added same code under the trait file

--- app/Http/Controllers/FetchTilesController.php
+++ app/Http/Controllers/FetchTilesController.php
@@ -33,63 +33,38 @@ class FetchTilesController extends Controller
         $endDate = $request->end_date;
 
         $apiUrl = "https://somany-backend.brndaddo.ai/api/v1/en_GB/products/autocomplete";
-        $recordsProcessed = 0;
-        $insertedCount = 0;
-        $updatedCount = 0;
-        $unchangedCount = 0;
 
-        $page = 1;
-        $perPage = 500; // Fetch 500 records per API call
+        $queryParams = http_build_query([
+            's' => $startDate,
+            'e' => $endDate,
+        ]);
 
-        do {
-            $queryParams = http_build_query([
-                's' => $startDate,
-                'e' => $endDate,
-                'page' => $page,
-                'limit' => $perPage,
-            ]);
+        $headers = [
+            'JWTAuthorization: Bearer ' . $getToken,
+        ];
+        // Use the trait function for GET request
+        $data = $this->makeGetRequest($apiUrl, $queryParams, $headers);
 
-            $headers = [
-                'JWTAuthorization: Bearer ' . $getToken,
-            ];
+        if (isset($data['error'])) {
+            return response()->json([
+                'error' => 'Unable to fetch total records: ' . $data['error'],
+            ], 500);
+        }
 
-            // Use the trait function for GET request
-            $data = $this->makeGetRequest($apiUrl, $queryParams, $headers);
-
-            if (isset($data['error'])) {
-                return response()->json([
-                    'error' => 'Unable to fetch total records: ' . $data['error'],
-                ], 500);
-            }
-
-            if (empty($data)) {
-                break; // Stop if there are no more records
-            }
+        if (empty($data)) {
+            exit(); // Stop if there are no more records
+        }
 
 
-            $totalRecords = count($data);
-            $recordsProcessed += $totalRecords;
-
-            // Insert or update database
-            $dbStats = $this->updateOrInsertMultiple($data, $endDate, $totalRecords);
-            $insertedCount += $dbStats['count'];
-            $updatedCount += $dbStats['updatedCount'];
-            $unchangedCount += $dbStats['unchangedCount'];
-
-            $page++; // Fetch the next page
-        } while ($totalRecords >= $perPage); // Keep fetching until there are no more records
-
-        $updatedMessage = Carbon::parse($endDate)->format('d M Y') . " (" . $recordsProcessed . ' records fetched)';
+        $totalRecords = count($data);
+        // Insert or update database
+        $dbStats = $this->updateOrInsertMultiple($data, $endDate, $totalRecords);
 
         return response()->json([
             'success' => true,
             'message' => 'Data processed successfully.',
-            'total_records' => $recordsProcessed,
-            'new_date' => $endDate,
-            'updated_message' => $updatedMessage,
-            'insertedCount' => $insertedCount,
-            'updatedCount' => $updatedCount,
-            'unchangedCount' => $unchangedCount,
+            'total_records' => $totalRecords,
+            'new_date' => $endDate
         ]);
     }
 
@@ -186,117 +161,5 @@ class FetchTilesController extends Controller
             'count' => $processedCount,
             'unchangedCount' => $unchangedCount,
         ];
-    }
-
-    protected function prepareTileData(array $product,$creation_time): array
-    {
-        $surface = strtolower($product['surface']);
-        $imageURL = ($product['image'] ) ?? $product['image_variation_1'];
-        return [
-            'name' => $product['product_name'] ?? null,
-            'shape' => $product['shape'] ?? 'square',
-            'width' => intval($product['size_wt']) ?? 0,
-            'height' => intval($product['size_ht']) ?? 0,
-            'size' => $product['size'] ?? null,
-            'surface' => $surface ?? null,
-            'finish' => $product['design_finish'] ?? null,
-            'file' => $this->fetchAndSaveImage($imageURL),
-            'image_variation_1' => $product['image_variation_1'] ?? null,
-            'image_variation_2' => $product['image_variation_2'] ?? null,
-            'grout' => ( $surface === "wall" || $surface === "floor" ) ? 1 : null,
-            'url' => $product['url'] ?? null,
-            'price' => $product['price'] ?? null,
-            'expProps' => json_encode([
-                'thickness' => $product['thickness'] ?? null,
-                'product code' => $product['design_finish'] ?? null,
-                'colour' => $product['color'] ?? null,
-                'category' => $product['brand_name'] ?? null,
-            ]), // Combined JSON field
-            'rotoPrintSetName' => str_replace(" FP", "", $product['product_name']) ?? null,
-            'access_level' => $product['access_level'] ?? null,
-            'sku' => $product['sku'] ?? null,
-            'application_room_area' => $product['application_room_area'] ?? null,
-            'brand' => $product['brand_name'] ?? null,
-            'sub_brand_1' => $product['sub_brand_1'] ?? null,
-            'innovation' => $product['innovation'] ?? null,
-            'sub_brand_2' => $product['sub_brand_2'] ?? null,
-            'color' => $product['color'] ?? null,
-            'poc' => $product['poc'] ?? null,
-            'thickness' => $product['thickness'] ?? null,
-            'tiles_per_carton' => $product['tiles_per_carton'] ?? null,
-            'avg_wt_per_carton' => $product['avg_wt_per_carton'] ?? null,
-            'coverage_sq_ft' => $product['coverage_sq_ft'] ?? null,
-            'coverage_sq_mt' => $product['coverage_sq_mt'] ?? null,
-            'pattern' => $product['pattern'] ?? null,
-            'plant' => $product['plant'] ?? null,
-            'service_geography' => $product['service_geography'] ?? null,
-            'unit_of_production' => $product['unit_of_production'] ?? null,
-            'yes_no' => $product['yes_no'] ?? null,
-            'record_creation_time' => $creation_time,
-            'deletion' => $product['deletion'] ?? null,
-            'from_api' => '1',
-            'updated_at' => now(),
-        ];
-    }
-
-    protected function fetchAndSaveImage($imageURL): JsonResponse|string
-    {
-        // Get tiles data
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $imageURL);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $this->getSSLVerfier()); // Ensure SSL verification is enabled
-
-        $imageContent = curl_exec($ch);
-        
-        // Get the content type of the image (optional: you can validate the type)
-        $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-
-        curl_close($ch);
-
-        if (!$imageContent || !str_starts_with($contentType, 'image/')) {
-            return response()->json([
-                'error' => 'Invalid image content',
-                'message' => 'Fetched content is not a valid image.',
-            ], 406);
-        }
-        
-        if (stristr($contentType, 'tiff')) {
-            $tempTiffPath = storage_path('temp_image.tiff');
-            file_put_contents($tempTiffPath, $imageContent);
-
-            // Convert TIFF to JPG using ImageMagick
-            $tempJpgPath = storage_path('temp_image.jpg');
-            exec("convert $tempTiffPath $tempJpgPath");
-
-            if (file_exists($tempJpgPath)) {
-                $imageContent = file_get_contents($tempJpgPath);
-                unlink($tempTiffPath);
-                unlink($tempJpgPath);
-            } else {
-                return response()->json([
-                    'error' => 'Failed to convert TIFF to JPG.',
-                ], 406);
-            }
-        }
-
-
-        $image_name = uniqid() . '.jpg';// Unique ID to avoid overwrite
-
-        // Generate a unique filename for the image
-        $fileName = 'tiles/' . $image_name;// Unique ID to avoid overwrite
-        $iconFilePath = 'tiles/icons/' . $image_name;
-        
-         // Use Intervention/Image to handle the image content
-        $image = InterventionImage::make($imageContent)
-            ->resize(800, null, function ($constraint) {
-                $constraint->aspectRatio();
-                $constraint->upsize();
-            });
-
-        Storage::disk('public')->put($iconFilePath, $image->encode());
-        Storage::disk('public')->put($fileName, $imageContent);
-
-        return $fileName;
     }
 }
--- app/Traits/ApiHelper.php
+++ app/Traits/ApiHelper.php
@@ -2,6 +2,11 @@
 
 namespace App\Traits;
 
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image as InterventionImage;
+
 trait ApiHelper
 {
     /**
@@ -64,7 +69,7 @@ trait ApiHelper
     /**
      * Get SSL Verifier (can be modified based on environment needs).
      */
-    private function getSSLVerifier()
+    private function getSSLVerifier(): bool
     {
         // Get the value of MY_CUSTOM_VAR from the .env file
         $customVar = config('app.curl'); // 'default_value' is the fallback in case MY_CUSTOM_VAR is not set
@@ -74,7 +79,7 @@ trait ApiHelper
     /**
      * Prepare headers for API requests.
      */
-    private function prepareHeaders($extraHeaders = [])
+    private function prepareHeaders($extraHeaders = []): array
     {
         $defaultHeaders = [
             "Content-Type: application/json",
@@ -82,6 +87,82 @@ trait ApiHelper
 
         return array_merge($defaultHeaders, $extraHeaders);
     }
+
+
+    protected function prepareTileData(array $product,$creation_time): array
+    {
+        // Check if 'design_finish' key is missing and log a warning
+        if (!isset($product['design_finish'])) {
+            \Log::warning("Missing key 'design_finish' for SKU: " . ($product['sku'] ?? 'Unknown'));
+            $product['design_finish'] = "GLOSSY";
+        }
+
+        if( !isset($product['brand_name'])){
+            \Log::warning("Missing key 'brand_name' for SKU: " . ($product['sku'] ?? 'Unknown'));
+            $product['brand_name'] = "Duragres";
+        }
+
+        $surface = strtolower($product['surface']);
+        $imageURL = ($product['image'] ) ?? $product['image_variation_1'];
+
+        $parsedUrl = parse_url($imageURL);
+        $filePath = $parsedUrl['path']; // This gets the file path without query parameters
+        // Get the file extension (e.g., tiff, psd, jpg)
+        $fileType = strtolower(pathinfo($filePath, PATHINFO_EXTENSION));
+        // Check if the file is TIFF or PSD and set the image to null
+        if (in_array($fileType, ['tif', 'psd' ,'tiff'])) {
+            $fileName = null;
+        } else {
+            $fileName = $this->fetchAndSaveImage($imageURL);
+        }
+
+        return [
+            'name' => $product['product_name'] ?? null,
+            'shape' => $product['shape'] ?? 'square',
+            'width' => intval($product['size_wt']) ?? 0,
+            'height' => intval($product['size_ht']) ?? 0,
+            'size' => $product['size'] ?? null,
+            'surface' => $surface ?? null,
+            'finish' => $product['design_finish'] ?? null,
+            'file' => $this->fetchAndSaveImage($imageURL),
+            'image_variation_1' => $product['image_variation_1'] ?? null,
+            'image_variation_2' => $product['image_variation_2'] ?? null,
+            'grout' => ( $surface === "wall" || $surface === "floor" ) ? 1 : null,
+            'url' => $product['url'] ?? null,
+            'price' => $product['price'] ?? null,
+            'expProps' => json_encode([
+                'thickness' => $product['thickness'] ?? null,
+                'product code' => $product['design_finish'] ?? null,
+                'colour' => $product['color'] ?? null,
+                'category' => $this->mapCategoryType(strtolower($product['brand_name'])) ?? null,
+            ]), // Combined JSON field
+            'rotoPrintSetName' => str_replace(" FP", "", $product['product_name']) ?? null,
+            'access_level' => $product['access_level'] ?? null,
+            'sku' => $product['sku'] ?? null,
+            'application_room_area' => $product['application_room_area'] ?? null,
+            'brand' => $product['brand_name'] ?? null,
+            'sub_brand_1' => $product['sub_brand_1'] ?? null,
+            'innovation' => $product['innovation'] ?? null,
+            'sub_brand_2' => $product['sub_brand_2'] ?? null,
+            'color' => $product['color'] ?? null,
+            'poc' => $product['poc'] ?? null,
+            'thickness' => $product['thickness'] ?? null,
+            'tiles_per_carton' => $product['tiles_per_carton'] ?? null,
+            'avg_wt_per_carton' => $product['avg_wt_per_carton'] ?? null,
+            'coverage_sq_ft' => $product['coverage_sq_ft'] ?? null,
+            'coverage_sq_mt' => $product['coverage_sq_mt'] ?? null,
+            'pattern' => $product['pattern'] ?? null,
+            'plant' => $product['plant'] ?? null,
+            'service_geography' => $product['service_geography'] ?? null,
+            'unit_of_production' => $product['unit_of_production'] ?? null,
+            'yes_no' => $product['yes_no'] ?? null,
+            'record_creation_time' => $creation_time,
+            'deletion' => $product['deletion'] ?? null,
+            'from_api' => '1',
+            'updated_at' => now(),
+        ];
+    }
+
 
     private function mapFinishType($designFinish): string
     {
@@ -126,5 +207,67 @@ trait ApiHelper
         ];
 
         return $mapping[$brand_name] ?? $brand_name; // Default to original value if not in mapping
+    }
+
+
+    /**
+     * @throws Exception
+     */
+
+    protected function fetchAndSaveImage($imageURL): JsonResponse|string
+    {
+        // Get tiles data
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $imageURL);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $this->getSSLVerfier()); // Ensure SSL verification is enabled
+
+        $imageContent = curl_exec($ch);
+
+        // Get the content type of the image (optional: you can validate the type)
+        $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+        curl_close($ch);
+
+        if (!$imageContent || !str_starts_with($contentType, 'image/')) {
+            return response()->json([
+                'error' => 'Invalid image content',
+                'message' => 'Fetched content is not a valid image.',
+            ], 406);
+        }
+        if (stristr($contentType, 'tif')) {
+            $tempTiffPath = storage_path('temp_image.tiff');
+            file_put_contents($tempTiffPath, $imageContent);
+
+            // Convert TIFF to JPG using ImageMagick
+            $tempJpgPath = storage_path('temp_image.jpg');
+            exec("convert $tempTiffPath $tempJpgPath");
+
+            if (file_exists($tempJpgPath)) {
+                $imageContent = file_get_contents($tempJpgPath);
+                unlink($tempTiffPath);
+                unlink($tempJpgPath);
+            } else {
+                return response()->json([
+                    'error' => 'Failed to convert TIFF to JPG.',
+                ], 406);
+            }
+        }
+
+        $image_name = uniqid() . '.jpg';// Unique ID to avoid overwrite
+
+        // Generate a unique filename for the image
+        $fileName = 'tiles/' . $image_name;// Unique ID to avoid overwrite
+        $iconFilePath = 'tiles/icons/' . $image_name;
+
+        // Resize the image to 100x100 and store it in the 'icons' folder
+        $image = InterventionImage::make($imageContent)->resize(100, 100);  // Resize to 100x100
+
+        // Store the resized image in the 'icons' folder inside the 'public' storage folder
+        Storage::disk('public')->put($iconFilePath, $image->encode());
+
+        // Store the image content in the public disk (public/tiles folder in storage)
+        Storage::disk('public')->put($fileName, $imageContent);
+        return $fileName;
     }
 }
