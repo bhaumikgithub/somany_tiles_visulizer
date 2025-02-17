21a5be79befe4cc30bffb5c24ed97ceff065ed00
Vikesha <Vikesha.gandhi@protovate.com>
2025-02-17 15:18:28 +0530

    data shown

--- public/js/room/custom.js
+++ public/js/room/custom.js
@@ -178,6 +178,14 @@ function toggleShareElements() {
 }
 $(window).on('load', function () {
 
+    if (isCanvasFullscreen()) {
+        $(".cmn-room-btn").css('visibility', 'visible');
+        $(".share-div").css('visibility', 'visible');
+
+        AdjustCanvasWidthHeight();
+    }
+    // checkCanvasVisibility();
+
     interval = setInterval(function () {
 
         if ($("#sourceLoadProgressBarContainer").length > 0) {
@@ -208,7 +216,7 @@ $(window).on('load', function () {
 
     $(document).on('click', '.filter-click', function() {
         alert("Click");
-        console.log($(this).attr("id"));
+        //console.log($(this).attr("id"));
     });
 });
 
@@ -503,10 +511,25 @@ function customFilterManagement(){
     htmlStr+="</ul>";
 
     $("#topPanelNavFilter").html(htmlStr);
+    $(".checkboxClass").prop("checked",false);
+    badLogicForCheckbox = true;
 
-
+    -body_Finishes
 }
-
+var badLogicForCheckbox = true;
+function totalFilterCheckboxesChecked(p_category){
+    console.log("p_category = " + p_category);
+    var checked = $(".checkboxClass");
+    var totalCheckboxes = checked.length;
+    var count = 0;
+    for(var i=0;i<totalCheckboxes;i++){
+        var obj = $(checked).eq(i);
+        if($(obj).prop("checked")){
+            count++;
+        }
+    }
+    return count;
+}
 function clickFilterCategory(p_filterName){
     $('.-body').hide();
     $('#-body_' + p_filterName).css({"display":"flex"});
