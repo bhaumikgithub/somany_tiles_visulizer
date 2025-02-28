64f156cff7f4daa2b07a2c7304f364b852ce490e
Kinjal Upadhyay <kinjal.upadhyay@protovate.com>
2025-02-28 11:47:40 +0530

    When session is destoryed it should again ask for pincode - fixed

--- app/Http/Controllers/AddToPdfRoomsController.php
+++ app/Http/Controllers/AddToPdfRoomsController.php
@@ -219,12 +219,16 @@ class AddToPdfRoomsController extends Controller
 
     public function pdfSummary(Request $request , $randomKey): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
     {
-        $pincode = Session::get('pincode'); // Store the pincode temporarily
+        $pincode = Session::get('pincode'); // Store the pin code temporarily
+
+        //Update pin code in cart summary page
+        $getCartId = Cart::where('random_key',$randomKey)->first();
+        $getCartId->pincode = $pincode;
+        $getCartId->update();
 
         Session::flush(); // Clears all session data
 
-        Session::put('pincode', $pincode); // Restore the pincode session
-            
+
         // Optionally, regenerate session ID for the user
         $request->session()->regenerate();  // This generates a new session ID
 
@@ -239,7 +243,7 @@ class AddToPdfRoomsController extends Controller
                 'showrooms' => [],
             ];
         }        // Retrieve the pincode from the session
-        $pincode = session('pincode', null); // Default to null if not set
+        $pincode = $getCartId->pincode ?? null;
 
 
         $groupedTiles = $this->getProcessedTiles($allProduct);
@@ -253,10 +257,6 @@ class AddToPdfRoomsController extends Controller
             $isReadOnly = true;
             $upform_data = $savedUserpdfData->first();
         }
-
-        // if($isReadOnly){
-        //     $upform_data = UserPdfData::where([['unique_id',$randomKey],['name',base64_decode(request()->query('name'))]])->get()->first();
-        // }
 
         $cc_date = $getCartId->created_at->format('d-m-y');
         return view('pdf.cart_summary',compact('allProduct','randomKey','groupedTiles','upform_data','isReadOnly','cc_date','groupedTiles','pincode','userShowroomInfo'));
@@ -315,7 +315,7 @@ class AddToPdfRoomsController extends Controller
             'contact_no' => $request->mobileNumber,
             'state' => $request->state,
             'city' => $request->city,
-            'pin_code' => session('pincode', null)// Default to null if not set
+            'pin_code' => $getCartId->pincode ?? null
         ];
 
         $firstProduct = $allProduct->first(); // This returns the first CartItem model
