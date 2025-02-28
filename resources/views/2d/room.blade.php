f47b11a448bc8d64defe5a1d7a3b00a5bae0cbc8
Kinjal Upadhyay <kinjal.upadhyay@protovate.com>
2025-02-28 10:16:05 +0530

    2 reported issues fixes

--- resources/views/2d/room.blade.php
+++ resources/views/2d/room.blade.php
@@ -26,11 +26,8 @@
                         class="fa-solid fa-xmark"></i></button>
         </div>
 
-
-        <!-- <a href="{{ url('listing/' . @$room_type) }}" class="cmn-room-btn back-btn d-flex flex-wrap align-items-center"> <span class="span-icon"><i class="fa-solid fa-arrow-left"></i>
-                                </span> <span class="btn-text-set">Back </span> </a> -->
         <div class=" cmn-room-btn cmn-room-back-btn back-btn">
-            <a href="{{ url('/') }}"> <span class="span-icon"><i class="fa-solid fa-arrow-left"></i>
+            <a href="{{ url('listing/' . @$room_type) }}"> <span class="span-icon"><i class="fa-solid fa-arrow-left"></i>
                 </span></a>
             <p class="btn-text-set-back">Back </p>
         </div>
