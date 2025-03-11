a662be078e332e8abb64271fde796a88e9f7f650
Kinjal Upadhyay <kinjal.upadhyay@protovate.com>
2025-03-11 13:29:39 +0530

    working on size refactor

---
+++ app/Console/Commands/ImageSizeRefactor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ImageSizeRefactor extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'image-size-refactor';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This commands change orientation based on image size';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
