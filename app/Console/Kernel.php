05617add46f0fc4f57d7727520b13771c08abb81
Kinjal Upadhyay <kinjal.upadhyay@protovate.com>
2025-04-25 09:55:13 +0530

    change the time to test cron job run or not

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('task:nightly')->dailyAt('23:59');
+        $schedule->command('task:nightly')->dailyAt('10:15');
     }
 
     /**
