b044ba0cd8731a50dda09dac3350dce940539345
Kinjal Upadhyay <kinjal.upadhyay@protovate.com>
2025-04-25 13:17:23 +0530

    cron job runs at 8:00 am

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('task:nightly')->dailyAt('07:45');
+        $schedule->command('task:nightly')->dailyAt('08:00');
     }
 
     /**
