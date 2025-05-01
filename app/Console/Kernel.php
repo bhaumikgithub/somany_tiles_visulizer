5ae3086ae69fde726f7ebccf7728e5e2a6022985
Kinjal Upadhyay <kinjal.upadhyay@protovate.com>
2025-05-01 11:37:08 +0530

    Run cron job at 11:45 am to test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('task:nightly')->dailyAt('18:29');
+        $schedule->command('task:nightly')->dailyAt('06:15');
     }
 
     /**
